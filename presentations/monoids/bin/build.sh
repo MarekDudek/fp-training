763156e667e1ac5f57ec1e7e74440c04dc5bd28d
Marek Dudek <marek.dudek@gmail.com>
2018-09-30 16:16:01 +0200

    separate document for blog post

--- presentations/monoids/bin/build.sh
+++ presentations/monoids/bin/build.sh
@@ -6,4 +6,8 @@ IFS=$'\n\t'
 DIRNAME=$(dirname "$0")
 
 
-pdflatex ${DIRNAME}/../slides/monoids.tex
+pdflatex ${DIRNAME}/../slides/monoids-slides.tex
+pdflatex ${DIRNAME}/../slides/monoids-slides.tex
+
+pdflatex ${DIRNAME}/../blog/monoids-post.tex
+pdflatex ${DIRNAME}/../blog/monoids-post.tex
