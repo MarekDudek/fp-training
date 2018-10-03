83ffc0ed0c4fa1b6d4ae3d7a047420a01aa7e5db
Marek Dudek <marek.dudek@gmail.com>
2018-10-03 21:25:09 +0200

    sum and product refactored

--- haskell/fp-training-haskell/test/SemigroupsSpec.hs
+++ haskell/fp-training-haskell/test/SemigroupsSpec.hs
@@ -3,6 +3,7 @@ module SemigroupsSpec where
 
 import Data.Semigroup
 import Data.List.NonEmpty
+import Control.Monad
 
 import Test.Hspec
 
@@ -10,6 +11,7 @@ import Test.Hspec
 main :: IO ()
 main = hspec spec
 
+nel = 1 :| [2, 3, 4, 5]
 
 spec :: Spec
 spec = 
@@ -19,30 +21,30 @@ spec =
                 getMin (Min 5) `shouldBe` 5
             it "of two" $
                 Min 2 <> Min 5 `shouldBe` Min 2 
-            let l = Min 5 :| Min 1 : Min 4 : Min 2 : Min 3 : [] 
+            let mins = fmap Min nel
             it "of non-empty list" $ 
-                sconcat l `shouldBe` Min 1 
+                sconcat mins `shouldBe` Min 1 
         describe "maximum of numbers" $ do
             it "wrapper" $
                 getMax (Max 5) `shouldBe` 5
             it "of two" $
                 Max 2 <> Max 5 `shouldBe` Max 5 
-            let l = Max 5 :| Max 1 : Max 4 : Max 2 : Max 3 : [] 
+            let maxs = fmap Max nel
             it "of non-empty list" $ 
-                sconcat l `shouldBe` Max 5
+                sconcat maxs `shouldBe` Max 5
         describe "addition of numbers" $ do
             it "wrapper" $
                 getSum (Sum 5) `shouldBe` 5
             it "of two" $
                 Sum 2 <> Sum 5 `shouldBe` Sum 7
-            let l = Sum 5 :| Sum 1 : Sum 4 : Sum 2 : Sum 3 : [] 
+            let sums = fmap Sum nel
             it "of non-empty list" $ 
-                sconcat l `shouldBe` Sum 15
+                sconcat sums `shouldBe` Sum 15
         describe "multiplication of numbers" $ do
             it "wrapper" $
                 getProduct (Product 5) `shouldBe` 5
             it "of two" $
                 Product 2 <> Product 5 `shouldBe` Product 10
-            let l = Product 5 :| Product 1 : Product 4 : Product 2 : Product 3 : [] 
+            let products = fmap Product nel
             it "of non-empty list" $ 
-                sconcat l `shouldBe` Product 120
+                sconcat products `shouldBe` Product 120
