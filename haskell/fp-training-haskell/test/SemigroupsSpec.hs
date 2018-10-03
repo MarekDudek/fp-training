74ac9328d5de1c2dbcd4c9d76b8cd39fe3280491
Marek Dudek <marek.dudek@gmail.com>
2018-10-03 21:14:55 +0200

    additon and multiplication of numbers

--- haskell/fp-training-haskell/test/SemigroupsSpec.hs
+++ haskell/fp-training-haskell/test/SemigroupsSpec.hs
@@ -30,3 +30,19 @@ spec =
             let l = Max 5 :| Max 1 : Max 4 : Max 2 : Max 3 : [] 
             it "of non-empty list" $ 
                 sconcat l `shouldBe` Max 5
+        describe "addition of numbers" $ do
+            it "wrapper" $
+                getSum (Sum 5) `shouldBe` 5
+            it "of two" $
+                Sum 2 <> Sum 5 `shouldBe` Sum 7
+            let l = Sum 5 :| Sum 1 : Sum 4 : Sum 2 : Sum 3 : [] 
+            it "of non-empty list" $ 
+                sconcat l `shouldBe` Sum 15
+        describe "multiplication of numbers" $ do
+            it "wrapper" $
+                getProduct (Product 5) `shouldBe` 5
+            it "of two" $
+                Product 2 <> Product 5 `shouldBe` Product 10
+            let l = Product 5 :| Product 1 : Product 4 : Product 2 : Product 3 : [] 
+            it "of non-empty list" $ 
+                sconcat l `shouldBe` Product 120
