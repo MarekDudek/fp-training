92e180ee2b3b97b17754f623cf4581e9f85bc910
Marek Dudek <marek.dudek@gmail.com>
2018-10-22 00:03:50 +0200

    comparison test for arithmetic mean semigroup

--- java/fp-training-java/src/test/java/fp/statistics/StatisticsSemigroupsTest.java
+++ java/fp-training-java/src/test/java/fp/statistics/StatisticsSemigroupsTest.java
@@ -1,5 +1,8 @@
 package fp.statistics;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.math3.stat.StatUtils;
+import org.apache.commons.math3.util.Precision;
 import org.junit.Test;
 
 import java.util.Optional;
@@ -8,6 +11,10 @@ import java.util.stream.Stream;
 import static fp.statistics.Average.average;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.quicktheories.QuickTheory.qt;
+import static org.quicktheories.generators.SourceDSL.arrays;
+import static org.quicktheories.generators.SourceDSL.doubles;
 
 public final class StatisticsSemigroupsTest {
 
@@ -21,5 +28,23 @@ public final class StatisticsSemigroupsTest {
         Optional<Average> average = Stream.of(a, b, c).reduce(Average::append);
         // then
         assertThat(average.get().getAverage(), is(3.3333333333333335));
+    }
+
+    @Test
+    public void arithmetic_mean_comparison_test() {
+        qt().
+                forAll(
+                        arrays().ofClass(doubles().between(0, 100), Double.class).withLength(100)
+                ).
+                checkAssert(
+                        ds -> {
+                            double avg1 = Stream.of(ds).
+                                    map(Average::average).
+                                    reduce(Average::append).
+                                    get().getAverage();
+                            double avg2 = StatUtils.mean(ArrayUtils.toPrimitive(ds));
+                            assertTrue(Precision.equals(avg1, avg2, 0.001));
+                        }
+                );
     }
 }
